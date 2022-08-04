c51d0245eed07afd6b449f9f46b836bac6858f57
Do Tuan <tuanm.dev@gmail.com>
2022-08-04 23:06:04 +0700

    update ipfs

--- src/services/ipfs.ts
+++ src/services/ipfs.ts
@@ -3,7 +3,7 @@ import API from '.';
 export default class IPFS {
     static async get(cid: string) {
         const response = await API.raw().get(
-            `https://ipfs.io/ipfs/${cid}`,
+            `${process.env.REACT_APP_IPFS_URL}/ipfs/${cid}`,
         );
         return response.data;
     }
