05f7c892400456d84909ba849cc2edd54487a16d
Do Tuan <tuanm.dev@gmail.com>
2022-08-09 22:48:47 +0700

    update gateway for ipfs-cat

--- src/services/ipfs.ts
+++ src/services/ipfs.ts
@@ -3,7 +3,7 @@ import API from '.';
 export default class IPFS {
     static async get(cid: string) {
         const response = await API.raw().get(
-            `${process.env.REACT_APP_IPFS_URL}/ipfs/${cid}`,
+            `${process.env.REACT_APP_IPFS_PUBLIC_URL}/ipfs/${cid}`,
         );
         return response.data;
     }
